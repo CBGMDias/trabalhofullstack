35cbc41bfea65d77704096416a40b4d84807fa3b
Cristhian Braun <114692497+CBGMDias@users.noreply.github.com>
2024-09-02 14:41:35 -0300

    Configurando BD e index.js
    Configurando o banco de dados e realizando a migração
    Adicionando o index.js para as rotas na raiz do projeto

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const { Sequelize } = require('sequelize');
+const db = require('./models');
+
+const app = express();
+const PORT = 3000;
+
+// Middleware para tratar JSON
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Servidor rodando...');
+});
+
+// Rota post pessoas
+app.post('/pessoas', async (req, res) => {
+  try {
+    const { nome, cpf, telefone } = req.body;
+    const novaPessoa = await db.Pessoa.create({ nome, cpf, telefone });
+    res.status(201).json(novaPessoa);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao criar a pessoa' });
+  }
+});
+
+// Rota get pessoas
+app.get('/pessoas', async (req, res) => {
+  try {
+    const pessoas = await db.Pessoa.findAll();
+    res.status(200).json(pessoas);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao buscar pessoas' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta http://localhost:${PORT}`);
+});
